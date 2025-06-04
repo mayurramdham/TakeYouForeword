5e1866b92940a35f2cace1a70aef79881326ba22
mayur ramdham <mayurramdham145@gmail.com>
2025-06-05 00:30:24 +0530

    done palindrome number

---
+++ Numbers/isPalindrome/isPalindrome/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace isPalindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+             bool IsPalindrome(int x)
+            {
+
+                int rem = 0;
+                int sum = 0;
+                int newnum = 0;
+                newnum = x;
+                while (x > 0)
+                {
+                    rem = x % 10;
+                    x = x / 10;
+                    sum = sum * 10 + rem;
+                }
+
+                if (newnum == sum)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+           
+        
+    }
+    }
+}
