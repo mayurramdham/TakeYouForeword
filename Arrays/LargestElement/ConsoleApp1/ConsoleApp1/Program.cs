f5957cc141184c963e70f59f30f3aef6aa9463be
mayur ramdham <mayurramdham145@gmail.com>
2025-06-05 23:53:31 +0530

    done largest and second largest

---
+++ Arrays/LargestElement/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[] arr = { 1, 5, 9, 5, 10, 5, 6,15,5,13,6 };
+            Console.WriteLine("secondLargest number is " + SecondLargest(arr));
+        }
+        public static int Largest(int[] arr)
+        {
+            int largest = arr[0];
+            int secondLargest = 0;
+            for(int i=0;i<arr.Length;i++)
+            {
+                if (arr[i]>largest)
+                {
+                    largest = arr[i];
+                }
+            }
+
+            for(int i=0;i<arr.Length;i++)
+            {
+                if (arr[i]!=largest)      //
+                {
+                   
+                        secondLargest = arr[i];
+                    
+                }
+            }
+            return secondLargest;
+
+        }
+        public static int SecondLargest(int[] arr)
+        {
+            int lar = arr[0];
+            int secondLar = 0;
+            for(int i=0;i<arr.Length;i++)
+            {
+                if (arr[i]>lar)
+                {
+                    secondLar = lar;
+                    lar = arr[i];
+                }
+                else if (arr[i]>secondLar)
+                {
+                    secondLar = arr[i];
+                }
+            }
+            return secondLar;
+        }
+       
+    }
+}
